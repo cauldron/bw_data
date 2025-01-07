0ae9e224bd04ee0b3515534999103086ebc3b5cf
Chris Mutel <cmutel@gmail.com>
2025-01-07 16:00:13 +0100

    Update tests to support Postgres via Docker
    Uses testcontainers and changes decorator to pytest parameterized fixture which runs on both SQLite and Postgres.
    
    Testcontainers requires a working installation of Docker. See https://java.testcontainers.org/supported_docker_environment/

--- bw2data/tests.py
+++ bw2data/tests.py
@@ -1,4 +1,5 @@
 import atexit
+import os
 import random
 import shutil
 import string
@@ -6,7 +7,12 @@ import tempfile
 import unittest
 from pathlib import Path
 
-import wrapt
+try:
+    from testcontainers.postgres import PostgresContainer
+except ImportError:
+    PostgresContainer = None
+
+import pytest
 
 from bw2data import config, databases, geomapping, methods
 from bw2data.project import projects
@@ -41,15 +47,42 @@ class BW2DataTest(unittest.TestCase):
         self.assertTrue("default" not in projects)
 
 
-@wrapt.decorator
-def bw2test(wrapped, instance, args, kwargs):
+@pytest.fixture(params=[True, False])
+def bw_test_fixture(request, tmp_path) -> None:
     config.dont_warn = True
     config.is_test = True
-    tempdir = Path(tempfile.mkdtemp())
     project_name = "".join(random.choices(string.ascii_lowercase, k=18))
+    if request.param and PostgresContainer:
+        postgres = PostgresContainer("postgres:16")
+        postgres.start()
+
+        def remove_container():
+            postgres.stop()
+
+            del os.environ['BW_DATA_POSTGRES_URL']
+            del os.environ['BW_DATA_POSTGRES_PORT']
+            del os.environ['BW_DATA_POSTGRES_USER']
+            del os.environ['BW_DATA_POSTGRES_PASSWORD']
+            del os.environ['BW_DATA_POSTGRES_DATABASE']
+            del os.environ['BW_DATA_POSTGRES']
+
+        request.addfinalizer(remove_container)
+
+        os.environ['BW_DATA_POSTGRES'] = "1"
+        os.environ['BW_DATA_POSTGRES_URL'] = postgres.get_container_host_ip()
+        os.environ['BW_DATA_POSTGRES_PORT'] = postgres.get_exposed_port(5432)
+        os.environ['BW_DATA_POSTGRES_USER'] = postgres.username
+        os.environ['BW_DATA_POSTGRES_PASSWORD'] = postgres.password
+        os.environ['BW_DATA_POSTGRES_DATABASE'] = postgres.dbname
+    else:
+        os.environ['BW_DATA_POSTGRES'] = ""
+
+        def remove_envvar():
+            del os.environ['BW_DATA_POSTGRES']
+
+        request.addfinalizer(remove_envvar)
+
     projects.change_base_directories(
-        base_dir=tempdir, base_logs_dir=tempdir, project_name=project_name, update=False
+        base_dir=tmp_path, base_logs_dir=tmp_path, project_name=project_name, update=False
     )
     projects._is_temp_dir = True
-    atexit.register(shutil.rmtree, tempdir)
-    return wrapped(*args, **kwargs)
